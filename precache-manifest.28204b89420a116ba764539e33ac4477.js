b73c5c6d30741abe141f14120e4f304131b28bd6
gaur.garima99@gmail.com <80620057+ProGarima@users.noreply.github.com>
2023-09-11 18:27:03 +0530

    Updates

---
+++ precache-manifest.28204b89420a116ba764539e33ac4477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3051cd4d6770afb85983",
+    "url": "/textutils/static/css/main.dd2051ff.chunk.css"
+  },
+  {
+    "revision": "3051cd4d6770afb85983",
+    "url": "/textutils/static/js/main.3051cd4d.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/textutils/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "839885bdb393db6476ee",
+    "url": "/textutils/static/js/runtime~main.839885bd.js"
+  },
+  {
+    "revision": "82db2ea35da19ea3a5c2414626d3e4ec",
+    "url": "/textutils/index.html"
+  }
+];
